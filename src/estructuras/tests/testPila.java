8d777e8a78f7e072338150b49174210f9ae6db9e
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2021-01-15 21:06:06 -0300

    Create testPila.java
    Clase testPila para comprobar funcionamiento del TDA Pila

---
+++ src/estructuras/tests/testPila.java
@@ -0,0 +1,25 @@
+package estructuras.tests;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import estructuras.lineales.dinamicas.Pila;
+
+public class testPila {
+    public static void main(String[] args) {
+        Pila pila;
+        Pila pila2;
+        BufferedReader entradaDatos;
+        try {
+            entradaDatos = new BufferedReader(new FileReader("D:\\Documentos\\Facultad\\2°Año\\1°Semestre\\Estructura de Datos\\Códigos\\TP_Final\\TPO\\src\\estructuras\\tests\\datos.txt"));
+            String cad = entradaDatos.readLine();
+            System.out.println("\n"+cad);
+            entradaDatos.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
