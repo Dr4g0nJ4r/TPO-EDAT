b11ceb9d7d096eb9d126fb3a5abeb4ae2d09216e
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2021-01-21 15:28:44 -0300

    Correcciones en los test Pila y Cola
    error con scanner.nextInt()
    
    Se debe agregar un nextLine() luego de esta instrucción para tomar el retorno de carro:
    
    Ejemplo:
    
    scanner.nextInt()
    scanner.nextLine()

--- src/estructuras/tests/testCola.java
+++ src/estructuras/tests/testCola.java
@@ -7,71 +7,68 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-//import estructuras.lineales.dinamicas.Pila;
-import estructuras.lineales.estaticas.Pila;
-public class testPila {
-    /**Método main de la clase TestPila. Levanta por defecto un lote de prueba y genera el menú */
+//import estructuras.lineales.dinamicas.Cola;
+import estructuras.lineales.estaticas.Cola;
+public class testCola {
     private static Scanner scanner;
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        ArrayList<Cola> arrayCola = new ArrayList<>();
+        Cola cola = new Cola();
+        Cola cola2 = new Cola();
+        preCargarDatos(cola);
+        preCargarDatos(cola2);
+        arrayCola.add(cola);
+        arrayCola.add(cola2);
         scanner = new Scanner(System.in);
-        Pila pila = new Pila();
-        Pila pila2 = new Pila();
-        preCargarDatos(pila);
-        preCargarDatos(pila2);
-        ArrayList<Pila> arrayPilas = new ArrayList<>();
-        arrayPilas.add(pila);
-        arrayPilas.add(pila2); 
-        System.out.println(pila.toString());
-        System.out.println(pila2.toString());
         int opcionElegida;
-        int indexPilaActual = 0;
+        int indexCola = 0;
         do{
             opcionElegida = menu();
             switch (opcionElegida) {
                 case 1:
-                //agrega un elemento a la Pila
+                //Agrega un elemento al final de la Cola
                     System.out.print("Ingrese un nuevo nombre y apellido: ");
                     String nuevoElem = scanner.nextLine();
-                    System.out.println("Se apiló el elemento? -> " + arrayPilas.get(indexPilaActual).apilar(nuevoElem));
+                    System.out.println("Se agregó el elemento? -> " + arrayCola.get(indexCola).poner(nuevoElem));
                     break;
                 case 2:
-                //Obtiene el elemento tope de la pila.
-                    if(arrayPilas.get(indexPilaActual).esVacia())
+                //Obtiene el elemento situado enfrente de la Cola.
+                    if(arrayCola.get(indexCola).esVacia())
                     {
-                        System.out.println("La pila está vacía");
+                        System.out.println("La Cola está vacía");
                     }else{
-                        System.out.println("El elemento tope es " + arrayPilas.get(indexPilaActual).obtenerTope());
+                        System.out.println("El elemento frente es " + arrayCola.get(indexCola).obtenerFrente());
                     }
                     break;
                 case 3:
-                //retira el elemento tope de la pila
-                    if(!arrayPilas.get(indexPilaActual).esVacia())
+                //Retira el elemento del frente de la Cola
+                    if(!arrayCola.get(indexCola).esVacia())
                     {
-                        System.out.println("Se retiró el elemento? -> " + arrayPilas.get(indexPilaActual).desapilar());
+                        System.out.println("Se retiró el elemento? -> " + arrayCola.get(indexCola).sacar());
                     }
                     break;
                 case 4:
-                //Consulta si la pila está vacia. True si, false no
-                    System.out.println("La pila está vacía? : " + arrayPilas.get(indexPilaActual).esVacia());
+                //Consulta si la Cola está vacia. True si, false no
+                    System.out.println("La Cola está vacía? : " + arrayCola.get(indexCola).esVacia());
                     break;
                 case 5:
-                //Clona la pila y crea una nueva. La almacena en arrayPila
-                    arrayPilas.add(arrayPilas.get(indexPilaActual).clone());
-                    System.out.println("Pila Clonada con éxito");
+                //Clona la Cola y crea una nueva. La almacena en arrayCola
+                    arrayCola.add(arrayCola.get(indexCola).clone());
+                    System.out.println("Cola Clonada con éxito");
                     break;
                 case 6:
-                //Visualizar la pila
-                    System.out.println(arrayPilas.get(indexPilaActual).toString());
+                //Visualizar la Cola
+                    System.out.println(arrayCola.get(indexCola).toString());
                     break;
                 case 7:
-                //Cambiar de pila en arrayPila
-                    indexPilaActual = (indexPilaActual + 1) % arrayPilas.size();
-                    System.out.println("Posición actual -> " + indexPilaActual);
+                //Cambiar de Cola en arrayCola
+                    indexCola = (indexCola + 1) % arrayCola.size();
+                    System.out.println("Posición actual -> " + indexCola);
                     break;
                 case 8:
-                //Vacía la Pila
-                    arrayPilas.get(indexPilaActual).vaciar();
+                //Vacía la Cola
+                    arrayCola.get(indexCola).vaciar();
                     break;
                 case 0:
                 //Cierre del programa
@@ -90,14 +87,14 @@ public class testPila {
     private static int menu(){
         int opcion;
         String opciones = "MENÚ \n";
-        opciones += "1. Agregar elemento a la Pila \n";
-        opciones += "2. Obtener elemento de la Pila \n";
-        opciones += "3. Sacar tope \n";
-        opciones += "4. Pila está vacia? \n";
-        opciones += "5. Clonar pila \n";
-        opciones += "6. Ver Pila \n";
-        opciones += "7. Cambiar de Pila \n";
-        opciones += "8. Vaciar pila\n";
+        opciones += "1. Agregar elemento a la Cola \n";
+        opciones += "2. Obtener elemento de la Cola \n";
+        opciones += "3. Sacar elemento \n";
+        opciones += "4. Cola está vacia? \n";
+        opciones += "5. Clonar Cola \n";
+        opciones += "6. Ver Cola \n";
+        opciones += "7. Cambiar de Cola \n";
+        opciones += "8. Vaciar Cola\n";
         opciones += "0. Cerrar programa \n";
         System.out.println(opciones);
         opcion = (int) scanner.nextInt();
@@ -106,7 +103,7 @@ public class testPila {
     }
 
     /**Precarga de datos desde archivo .txt externo */
-    private static void preCargarDatos(Pila pila) {
+    private static void preCargarDatos(Cola cola) {
         ArrayList<String> nombres = new ArrayList<>();
         BufferedReader reader;
         try {
@@ -122,7 +119,7 @@ public class testPila {
         while(nombres.size() > 0)
         {
             pos =numeroAleatorio.nextInt(nombres.size());
-            pila.apilar(nombres.get(pos));
+            cola.poner(nombres.get(pos));
             nombres.remove(pos);
         }
         } catch (IOException e) {
