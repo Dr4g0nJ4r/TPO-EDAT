820bd1abd5f68ce3a5ff1fd90b5c73009d0a3471
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2021-01-28 17:33:55 -0300

    Update NodoGen.java
    Se implementa NodoGen

--- src/estructuras/jerarquicas/dinamicas/NodoGen.java
+++ src/estructuras/jerarquicas/dinamicas/NodoGen.java
@@ -1,5 +1,46 @@
 package estructuras.jerarquicas.dinamicas;
 
+/**Estructura básica del Árbol Genérico. Contiene:
+ * 
+ *  - un elemento
+ *  - un enlace al nodo hijo situado más a la izquierda
+ *  - un enlace al nodo hermano
+ */
 public class NodoGen {
-    
+    private Object elemento;
+    private NodoGen hijoIzquierdo;
+    private NodoGen hermano;
+
+    /** Constructor. Retorna una instancia de NodoGen con el elemento pasado por parámetro y sin enlaces */
+    public NodoGen(Object elem)
+    {
+        this.elemento = elem;
+        this.hijoIzquierdo = null;
+        this.hermano = null;
+    }
+
+    public Object getElemento() {
+        return this.elemento;
+    }
+
+    public void setElemento(Object elemento) {
+        this.elemento = elemento;
+    }
+
+    public NodoGen getHijoIzquierdo() {
+        return this.hijoIzquierdo;
+    }
+
+    public void setHijoIzquierdo(NodoGen hijoIzquierdo) {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    public NodoGen getHermano() {
+        return this.hermano;
+    }
+
+    public void setHermano(NodoGen hermano) {
+        this.hermano = hermano;
+    }
+
 }
