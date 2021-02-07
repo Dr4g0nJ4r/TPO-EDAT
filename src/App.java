3ed48fd2c15f53a42c7df0af79a983ad2d3b92c7
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2021-02-07 13:58:26 -0300

    Cambios menu

--- src/App.java
+++ src/App.java
@@ -4,7 +4,160 @@ public class App {
     private static Scanner scanner;
     public static void main(String[] args) throws Exception {
         scanner = new Scanner(System.in);
-        Menu.menuPrincipal();
-    }
 
+        Character opcion;
+        System.out.println();
+        System.out.println("BIENVENIDO A 'DUNGEONS & STRUCTURES - THE LORD OF THE RINGS' EXPANSION");
+        System.out.println();
+        do{
+            opcion = Menu.menuPrincipal();
+            switch (opcion) {
+                case 'A':
+                    //ABM Jugador -> Submenu
+                    opcion = Menu.subMenuJugador();
+                    switch (opcion) {
+                        case '1':
+                            //Alta de Jugador
+                            break;
+                        case '2':
+                            //Modificación de Jugador
+                            break;
+                        case '3':
+                            //Baja de Jugador
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+            
+                case 'B':
+                    //ABM Items -> Submenu
+                    opcion = Menu.subMenuItem();
+                    switch (opcion) {
+                        case '1':
+                            //Alta de Item
+                            break;
+                        case '2':
+                            //Modificación de Item
+                            break;
+                        case '3':
+                            //Baja de Item
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 'C':
+                    //ABM Locación -> Submenu
+                    opcion = Menu.subMenuLocacion();
+                    switch (opcion) {
+                        case '1':
+                            //Alta de Locación
+                            break;
+                        case '2':
+                            //Modificación de Locación
+                            break;
+                        case '3':
+                            //Baja de Locación
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                    
+                case 'D':
+                    //Alta de un Jugador a un equipo
+                    break;
+                    
+                case 'E':
+                    //Creación automática de equipo
+                    break;
+                case 'F':
+                    //Paso a paso de una batalla
+                    break;
+            
+                case 'G':
+                    //Consulta sobre equipos
+                    break;
+                case 'H':
+                    //Consulta sobre items -> submenu consultaItem
+                    opcion = Menu.consultaItem();
+                    switch (opcion) {
+                        case '1':
+                            //Mostrar los items que puede comprar el jugador
+                            break;
+                        case '2':
+                            //Mostrar los items en un rango de precios
+                            break;
+                        case '3':
+                            //Mostrar propiedades del item
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                    
+                case 'I':
+                    //Consulta sobre jugador -> submenu consultaJugador
+                    opcion = Menu.consultaJugador();
+                    switch (opcion) {
+                        case '1':
+                            //Mostrar los datos de un jugador
+                            break;
+                        case '2':
+                            //Mostrar los jugadores que empiezan con 'xxx...' siendo x cualquier carácter
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                    
+                case 'J':
+                    //Consulta sobre locaciones -> submenu consultaLocacion
+                    opcion = Menu.consultaLocacion();
+                    switch (opcion) {
+                        case '1':
+                            //Mostrar las locaciones a las que se puede mover un equipo
+                            break;
+                        case '2':
+                            //Mostrar el camino de A a B más corto en km
+                            break;
+                        case '3':
+                            //Mostrar el camino de A a B más corto en locaciones
+                            break;
+                        case '4':
+                            //Mostrar los caminos de A a B posibles con menos de X km
+                            break;
+                        case '5':
+                            //Mostrar el camino de A a B más corto en locaciones y que no pase por C
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 'K':
+                    //Consultas generales -> submenu consultaGeneral
+                    opcion = Menu.consultaGeneral();
+                    switch (opcion) {
+                        case '1':
+                            //Ranking de jugadores con más individuales ganadas
+                            break;
+                        case '2':
+                            //Mostrar los items con solo un elemento disponible
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                    
+                case 'L':
+                    //Mostrar sistema
+                    break;
+
+                default:
+                System.out.println("¡¡ Ingrese una opción válida !!");
+                    break;
+            }
+        }while(opcion != 'M');
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 
 public class Menu {
     private static Scanner scanner = new Scanner(System.in);
-    public static String menuPrincipal()
+    public static Character menuPrincipal()
     {
-        System.out.println();
-        System.out.println("BIENVENIDO A 'DUNGEONS & STRUCTURES - THE LORD OF THE RINGS' EXPANSION");
+        System.out.println("MENU PRINCIPAL");
         System.out.println();
         System.out.println("A) ABM de jugadores");
         System.out.println("B) ABM de items");
@@ -19,72 +18,85 @@ public class Menu {
         System.out.println("J) Consulta sobre locaciones");
         System.out.println("K) Consultas generales");
         System.out.println("L) Mostrar sistema");
+        System.out.println("M) Salir del juego");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
+        System.out.println();
         return opcion;
     }
 
-    public static String subMenuJugador()
+    public static Character subMenuJugador()
     {
+        System.out.println("Menú Jugador");
         System.out.println();
         System.out.println("1) Alta de jugador");
         System.out.println("2) Modificación de jugador");
         System.out.println("3) Baja de jugador");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
-    public static String subMenuItem()
+    public static Character subMenuItem()
     {
+        System.out.println("Menú Item");
         System.out.println();
         System.out.println("1) Alta de ítem");
         System.out.println("2) Modificación de ítem");
         System.out.println("3) Baja de ítem");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
 
-    public static String subMenuLocacion()
+    public static Character subMenuLocacion()
     {
+        System.out.println("Menú Locación");
         System.out.println();
         System.out.println("1) Alta de locación");
         System.out.println("2) Modificación de locación");
         System.out.println("3) Baja de locación");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
 
-    public static String consultaItems()
+    public static Character consultaItem()
     {
+        System.out.println("Menú Consulta Item");
         System.out.println();
         System.out.println("1) Mostrar los items que puede comprar el jugador");
         System.out.println("2) Mostrar los items entre un rango de precios");
         System.out.println("3) Mostrar propiedades del ítem");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
 
-    public static String consultaJugador()
+    public static Character consultaJugador()
     {
+        System.out.println("Menú Consulta Jugador");
         System.out.println();
         System.out.println("1) Mostrar los datos de un jugador");
         System.out.println("2) Mostrar los jugadores que empiezan con '<texto>'");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
 
-    public static String consultaLocacion()
+    public static Character consultaLocacion()
     {
+        System.out.println("Menú Consulta Locación");
         System.out.println();
         System.out.println("1) Mostrar las locaciones a las que se puede mover un equipo");
         System.out.println("X) Dado dos locaciones A y B");
@@ -92,20 +104,23 @@ public class Menu {
         System.out.println("   3) Obtener el camino de A a B que pasa por la menor cantidad de locaciones");
         System.out.println("   4) Obtener todos los caminos para llegar de A a B con menos de una cantidad N de km");
         System.out.println("   5) Obtener el camino de A a B que pase por la menor cantidad de locaciones y no pase por la locación C");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
 
-    public static String consultaGeneral()
+    public static Character consultaGeneral()
     {
+        System.out.println("Menú Consulta General");
         System.out.println();
         System.out.println("1) Ranking de jugadores con más batallas individuales ganadas");
         System.out.println("2) Mostrar los items con solo un elemento disponible");
+        System.out.println("X) Volver al menú principal");
         System.out.println();
         System.out.print("Ingrese una opción:");
-        String opcion = scanner.nextLine();
+        Character opcion = scanner.nextLine().charAt(0);
         return opcion;
     }
 }
