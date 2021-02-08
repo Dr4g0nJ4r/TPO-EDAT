8809a95eef35d86fe05b08ceaea2ac38a06ed581
Dr4g0nJ4r <luis.keller@est.fi.uncoma.edu.ar>
2021-02-08 02:32:16 -0300

    Se agregan las estructuras especiales
    ColaPrioridad
    NodoCP

---
+++ src/estructuras/especiales/ColaPrioridad.java
@@ -0,0 +1,43 @@
+package estructuras.especiales;
+
+
+public class ColaPrioridad {
+    private NodoCP inicio;
+
+    public ColaPrioridad()
+    {
+        this.inicio = null;
+    }
+
+    public boolean insertar(Object elemento, int prioridad)
+    {
+        boolean exito = false;
+        
+        return exito;
+    }
+
+    public boolean eliminarFrente()
+    {
+        boolean exito = false;
+
+        return exito;
+    }
+
+    public Object obtenerFrente()
+    {
+        Object res = null;
+        if(!this.esVacia())
+        {
+            
+        }
+        return res;
+    }
+
+    public boolean esVacia()
+    {
+        boolean res = false;
+
+        return res;
+    }
+
+}
---
+++ src/estructuras/especiales/NodoCP.java
@@ -0,0 +1,8 @@
+package estructuras.especiales;
+
+import estructuras.lineales.dinamicas.Cola;
+
+public class NodoCP {
+    private int prioridad;
+    private Cola items;
+}
